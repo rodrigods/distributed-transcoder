2a6dee212ff970acc479850b4b30b5ad27c2c98c
Amaury Medeiros <amaurymedeiros@gmail.com>
2015-09-09 14:59:50 -0300

    Minor refactor in splitter.py

--- splitter.py
+++ splitter.py
@@ -1,16 +1,15 @@
 import argparse
 import uuid
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-fr', '--files-root', help='Path where the file')
 parser.add_argument('-fn', '--file-name', help='Name of the file to read')
 args = vars(parser.parse_args())
 
-f = open(args['files_root'] + '/' + args['file_name'], 'r')
-for line in f:
-    filename = uuid.uuid4().hex
-    new_file = open(args['files_root'] + '/' + filename, 'w')
-    new_file.write(args['file_name'] + ' ' + line)
-    new_file.close()
-
-    print filename
+with open(os.path.join(args['files_root'], args['file_name']), 'r') as input_file:
+    for line in input_file:
+        filename = uuid.uuid4().hex
+        with open(os.path.join(args['files_root'], filename), 'w') as output_file:
+            output_file.write('%s %s' % (args['file_name'], line))
+        print filename
